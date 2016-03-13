278dffe8c10b6cd39ead4e62448ee273f137e639
Fernando Gómez <gomezhyuuga@gmail.com>
2016-03-13 15:37:15 -0600

    Board helpful methods
    Board#blank_position
    Board#possible_moves

--- board.rb
+++ board.rb
@@ -1,6 +1,9 @@
 # 8 Puzzle Game
+require 'set'
+
 class Board
-  attr_reader :cells
+  attr_accessor :cells
+
   def initialize
     @cells = []
     cells = (1..8).to_a
@@ -8,9 +11,29 @@ class Board
     cells.shuffle!
     cells.each_slice(3) { |row| @cells << row }
   end
+
   def all_cells
     @cells.flatten
   end
+
+  def blank_position
+    index = self.all_cells.index " "
+
+    [index / 3, index % 3]
+  end
+
+  def possible_moves
+    moves = %i[up down left right].to_set
+    row, col = self.blank_position
+
+    moves.delete(:left) if col == 0
+    moves.delete(:right) if col == 2
+    moves.delete(:up) if row == 0
+    moves.delete(:down) if row == 2
+
+    moves
+  end
+
   def to_s
     @cells.map { |row| row.join "|" }.join "\n"
   end
