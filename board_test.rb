bbaf0358ac120fa6c762be9ba34a11c3d2fcc0c1
Fernando Gómez <gomezhyuuga@gmail.com>
2016-03-13 15:36:24 -0600

    Board TEST: blank_position

--- board_test.rb
+++ board_test.rb
@@ -21,6 +21,28 @@ class BoardTest < MiniTest::Unit::TestCase
     # Check for one empty space
     assert @board.all_cells.index " "
   end
+  def test_blank_position
+    @board.cells = self.mock_cells # [0][0]
+    assert_equal [0, 0], @board.blank_position
+    @board.cells = self.mock_cells_blank_at row: 0, col: 1
+    assert_equal [0, 1], @board.blank_position
+    @board.cells = self.mock_cells_blank_at row: 0, col: 2
+    assert_equal [0, 2], @board.blank_position
+
+    @board.cells = self.mock_cells_blank_at row: 1, col: 0
+    assert_equal [1, 0], @board.blank_position
+    @board.cells = self.mock_cells_blank_at row: 1, col: 1
+    assert_equal [1, 1], @board.blank_position
+    @board.cells = self.mock_cells_blank_at row: 1, col: 2
+    assert_equal [1, 2], @board.blank_position
+
+    @board.cells = self.mock_cells_blank_at row: 2, col: 0
+    assert_equal [2, 0], @board.blank_position
+    @board.cells = self.mock_cells_blank_at row: 2, col: 1
+    assert_equal [2, 1], @board.blank_position
+    @board.cells = self.mock_cells_blank_at row: 2, col: 2
+    assert_equal [2, 2], @board.blank_position
+  end
   def test_possible_moves
     # Blank = [0][0]
     @board.cells = self.mock_cells
@@ -57,11 +79,12 @@ class BoardTest < MiniTest::Unit::TestCase
       self.mock_cells_blank_at
     end
     def mock_cells_blank_at(row: 0, col: 0)
-      cells = [[0, 1, 2],
+      cells = [[" ", 1, 2],
                [3, 4, 5],
                [6, 7, 8]]
       n = cells[row][col]
       cells[row][col] = " "
       cells[0][0] = n
+      cells
     end
 end
