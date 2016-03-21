f4ffa6a14f4cfadcf6d35e3026f741e68f218164
Fernando Gómez <gomezhyuuga@gmail.com>
2016-03-21 01:48:29 -0600

    Beautify code

--- board.rb
+++ board.rb
@@ -24,7 +24,7 @@ class Board
     raise ArgumentError.new("Can't move that cell") unless self.can_move?(number)
 
     b_row, b_col = self.blank_position
-    c_row, c_col  = self.position_of(number)
+    c_row, c_col = self.position_of(number)
     @cells[b_row][b_col] = number
     @cells[c_row][c_col] = " "
 
@@ -33,18 +33,18 @@ class Board
 
   def can_move?(number)
     position = self.position_of(number)
-    moves = self.possible_moves
+    moves    = self.possible_moves
     moves.index(position) ? true : false
   end
 
   def possible_moves
     row, col = self.blank_position
-    moves = []
+    moves    = []
     self.possible_direction_moves.each do |move|
       case move
-      when :up then moves << [row - 1, col]
-      when :down then moves << [row + 1, col]
-      when :left then moves << [row, col - 1]
+      when :up    then moves << [row - 1, col]
+      when :down  then moves << [row + 1, col]
+      when :left  then moves << [row, col - 1]
       when :right then moves << [row, col + 1]
       end
     end
@@ -52,8 +52,8 @@ class Board
   end
 
   def possible_direction_moves
-    moves = %i[up down left right].to_set
     row, col = self.blank_position
+    moves    = %i[up down left right].to_set
 
     moves.delete(:left) if col == 0
     moves.delete(:right) if col == 2
@@ -64,7 +64,7 @@ class Board
   end
   def position_of(number)
     number = " " if number == :blank
-    index = self.all_cells.index number
+    index  = self.all_cells.index number
     [index / 3, index % 3]
   end
 
